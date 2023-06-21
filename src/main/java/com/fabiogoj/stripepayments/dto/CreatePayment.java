afe339a20326dc269b59ec91ccaa4da814f93392
fabiogoj <fabiogoj27@gmail.com>
2023-06-21 15:28:05 +0200

    configured stripe public key in application.properties

---
+++ src/main/java/com/fabiogoj/stripepayments/dto/CreatePayment.java
@@ -0,0 +1,13 @@
+package com.fabiogoj.stripepayments.dto;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CreatePayment {
+
+    @SerializedName("items")
+    Object[] items;
+
+    public Object[] getItems() {
+        return items;
+    }
+}
---
+++ src/main/java/com/fabiogoj/stripepayments/web/controller/PaymentController.java
@@ -0,0 +1,36 @@
+package com.fabiogoj.stripepayments.web.controller;
+
+import com.fabiogoj.stripepayments.dto.CreatePayment;
+import com.fabiogoj.stripepayments.dto.CreatePaymentResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.RequestEntity.post;
+
+@RestController
+public class PaymentController {
+
+    @PostMapping("/create-payment-intent")
+    public CreatePaymentResponse createPaymentIntent(CreatePayment createPayment) throws StripeException {
+
+        PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
+                .setAmount(15 + 100L)
+                .setCurrency("eur")
+                .setAutomaticPaymentMethods(
+                        PaymentIntentCreateParams.AutomaticPaymentMethods
+                                .builder()
+                                .setEnabled(true)
+                                .build()
+                )
+                .build();
+
+        // Create a PaymentIntent with the order amount and currency
+        PaymentIntent paymentIntent = PaymentIntent.create(params);
+
+        return new CreatePaymentResponse(paymentIntent.getClientSecret());
+
+    }
+}
